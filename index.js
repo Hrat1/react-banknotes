1b74d7ebbb06b41251d97dd43018502908dbe421
hrat1 <hrat.tahmazyan99@gmail.com>
2020-04-11 15:38:19 +0400

    Added parsers in package.json

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const {User} = require('./models/user');
+
 mongoose.connect('mongodb+srv://hrat:D5s4Q357a159O@cluster0-l01mn.mongodb.net/test?retryWrites=true&w=majority',
     {useNewUrlParser: true}).then(() => console.log("DB Connected"))
                             .catch(err => console.error(err));
 
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, userData) => {
+        if (err) return res.json({success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+    });
+
+
 });
-
 
 app.listen(5000);
