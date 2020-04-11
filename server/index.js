9b4a01d784da34c144a3fbba88dbba32fb18f826
hrat1 <hrat.tahmazyan99@gmail.com>
2020-04-11 21:20:08 +0400

    testing heroku

--- server/index.js
+++ server/index.js
@@ -17,6 +17,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.json({'hello': 'I am happy to deploy this application'})
+});
+
+
 app.get('/api/user/auth',auth, (req, res) => {
     res.status(200).json({
         _id: req._id,
